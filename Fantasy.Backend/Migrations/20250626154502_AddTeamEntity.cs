930ef8188eafcec4b877d9c59823f07bf2416e11
chugarsergio <chugarsergio@gmail.com>
2025-06-26 17:47:08 +0200

    chore(backend): db migration AddTeamEntity

---
+++ Fantasy.Backend/Migrations/20250626154502_AddTeamEntity.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Fantasy.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTeamEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "teams",
+                schema: "fantasy",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    image = table.Column<string>(type: "text", nullable: true),
+                    country_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_teams", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_teams_countries_country_id",
+                        column: x => x.country_id,
+                        principalSchema: "fantasy",
+                        principalTable: "countries",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_teams_country_id",
+                schema: "fantasy",
+                table: "teams",
+                column: "country_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_teams_name_country_id",
+                schema: "fantasy",
+                table: "teams",
+                columns: new[] { "name", "country_id" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "teams",
+                schema: "fantasy");
+        }
+    }
+}
